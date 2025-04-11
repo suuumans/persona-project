6319af449bc1092eafb1b8deec31e8a38ab25cd9
Suman Sarkar <152314759+suumans@users.noreply.github.com>
2025-04-11 19:29:20 +0530

    added vercel.json file and some other chamges

--- backend/main.py
+++ backend/main.py
@@ -113,7 +113,11 @@ app = FastAPI()
 # --- CORS Middleware ---
 origins = [
     "http://localhost:5173",
+    "https://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://127.0.0.1:5173",
+    "https://*.vercel.app",
+    "https://persona-project-frontend.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
